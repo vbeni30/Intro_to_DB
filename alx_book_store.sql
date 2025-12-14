b657a730f5afe08677a92b1a278d81e3b1cb5d04
vbeni <151450702+vbeni30@users.noreply.github.com>
2025-12-14 20:20:38 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,23 +1,20 @@
 -- alx_book_store.sql
 -- Database: alx_book_store
 
--- Drop the database if it exists
-DROP DATABASE IF EXISTS alx_book_store;
-
--- Create the database
-CREATE DATABASE alx_book_store;
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 -- Use the database
 USE alx_book_store;
 
 -- Create Authors table
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Create Books table
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
@@ -31,7 +28,7 @@ CREATE TABLE Books (
 );
 
 -- Create Customers table
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
@@ -39,7 +36,7 @@ CREATE TABLE Customers (
 );
 
 -- Create Orders table
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
@@ -51,7 +48,7 @@ CREATE TABLE Orders (
 );
 
 -- Create Order_Details table
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
